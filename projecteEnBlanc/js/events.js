c90ef78a076a012a1f5559b87242ea3b87c968b5
Pol Crespo <pol.crespo.hdez@gmail.com>
2023-09-22 18:52:04 +0200

    programa acabat (apriori)

--- projecteEnBlanc/js/events.js
+++ projecteEnBlanc/js/events.js
@@ -1,8 +1,29 @@
 function validarForm() {
     let numPersones = document.getElementById("persones").value;
-    let edat = document.getElementById("compte").value;
-    let animal = document.querySelector('input[name="servei"]:checked').value;
+    let compte = document.getElementById("compte").value;
+    let servei = document.querySelector('input[name="servei"]:checked').value;
     
   
-    return mostrarRespostes(numPersones + edat + animal);
-  }
+    return mostrarRespostes(numPersones, compte, servei);
+}
+
+function mostrarRespostes(numPersones, compte, servei) {
+    const propinaMin = 0.5;
+    let preuTotal;
+    switch (servei) {
+        case "acceptable":
+            preuTotal = compte * 5 / 100;
+            break;
+        case "genial":
+            preuTotal = compte * 10 / 100;
+            break;  
+        default:
+            preuTotal = compte;
+            break;
+    }
+    if (preuTotal / numPersones < propinaMin) {
+        alert("El preu de la propina és de: " + propinaMin + "€");
+    } else {
+        alert("El preu de la propina és de: " + preuTotal/numPersones + "€");
+    }
+}
