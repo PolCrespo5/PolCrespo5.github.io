9d9eff7b2c6d2541fbdb6eed16846b1460fb3d99
Pol Crespo <pol.crespo.hdez@gmail.com>
2023-09-28 16:03:43 +0200

    Calculador de propines acabada

--- projecteEnBlanc/js/events.js
+++ projecteEnBlanc/js/events.js
@@ -3,29 +3,45 @@ function validarForm() {
     let compte = document.getElementById("compte").value;
     let servei = document.getElementById("servei").value;
     
+    if (isNaN(compte)) {
+    	return mostrarError();
+    }
+    if (numPersones == "") {
+    	numPersones = 1;
+    }
     return mostrarRespostes(numPersones, compte, servei);
+}
+
+function isFloat(n){
+  return Number(n) === n && n % 1 !== 0;
+}
+
+function mostrarError() {
+	alert("Omple els camps correctament!");
+  return false;
 }
 
 function mostrarRespostes(numPersones, compte, servei) {
 		
-    const propinaMin = 0.5;
+    const propinaMin = 0.50;
     let preuTotal;
     switch (servei) {
         case "acceptable":
-            preuTotal = compte * 5 / 100;
+            preuTotal = compte * (5 / 100);
             break;
         case "genial":
-            preuTotal = compte * 10 / 100;
+            preuTotal = compte * (10 / 100);
             break;  
         default:
-            preuTotal = compte;
+            preuTotal = compte * (0 / 100);
             break;
     }
     if (preuTotal / numPersones < propinaMin) {
-        alert("El preu de la propina és de: " + propinaMin + "€");
+        alert("El preu de la propina és de: " + propinaMin*numPersones + "€\nCada persona haurà de pagar: " + propinaMin + "€");
     } else {
     		preuIndPropina = preuTotal/numPersones;
-        alert("El preu de la propina és de: " + preuIndPropina.toFixed(2) + "€");
+        alert("El preu de la propina és de: " + preuTotal.toFixed(2) + "€\nCada persona haurà de pagar: " + preuIndPropina.toFixed(2) + "€");
     }
+    document.getElementById("myForm").reset();
     return false;
 }
